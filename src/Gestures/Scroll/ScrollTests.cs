122983e5965d767d2593945f4004d444791162f7
QA-Automation-Testing-Demo <122603762+QA-Automation-Testing-Demo@users.noreply.github.com>
2024-07-28 14:51:08 +0300

    Update ScrollTests.cs

--- src/Gestures/Scroll/ScrollTests.cs
+++ src/Gestures/Scroll/ScrollTests.cs
@@ -12,7 +12,7 @@ namespace Scroll
         private AndroidDriver driver;
         private AppiumLocalService appiumLocalService;
 
-         var serverUri = new Uri("http://127.0.0.1:4723/wd/hub"); // Use the CI server's URL if different
+         var serverUri = new Uri("http://127.0.0.1:4723"); // Use the CI server's URL if different
 
      var androidOptions = new AppiumOptions
   {
