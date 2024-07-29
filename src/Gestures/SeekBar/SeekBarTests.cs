b2b591fb91ca36681154ffac27d285ebb25a7b4f
QA-Automation-Testing-Demo <122603762+QA-Automation-Testing-Demo@users.noreply.github.com>
2024-07-29 10:33:35 +0300

    Update SeekBarTests.cs

--- src/Gestures/SeekBar/SeekBarTests.cs
+++ src/Gestures/SeekBar/SeekBarTests.cs
@@ -34,8 +34,8 @@ namespace SeekBar
       App = appPath
   };
 
-    _driver = new AndroidDriver(serverUri, androidOptions);
-    _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); // Increase implicit wait
+    _driver = new AndroidDriver(serverUri, androidOptions, TimeSpan.FromMinutes(5)); // Increase command timeout
+    _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
 }
 
         private void ScrollToText(string text)
